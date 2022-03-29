f72ec1d14a16242b0a382183e1c856edefe7d628
jkenix <80328121+jkenix@users.noreply.github.com>
2022-03-29 23:10:08 +0300

    Изменения в коде для работы с новыми файлами. Теперь работа над проектом может быть начата. 29/03/22 23:09.

--- scripts.js
+++ scripts.js
@@ -1,1 +1,34 @@
 import '/styles/wooder_jk_parcel.scss';
+/* MENU */
+function showMenu() {
+    var w = $(window).width();
+    $(".menu_slider").css('width', w + "px");
+    $(".menu_link").css('opacity', 0.5);    
+}
+
+function closeMenu() {
+    var w = $(window).width();
+    $(".menu_slider").css('width', '0' + "px");
+    $(".menu_link").css('opacity', 0).fadeOut(1000, linear);
+}
+
+/* INFO */
+function showInfo() {
+    var h = $(window).height();
+    $(".info_wrapper").css('height', h + "px");
+    $(".information_block").css({'display': 'block', 'opacity': 1});
+    $(".information_block").css('display', 'block');
+}
+
+function closeInfo() {
+    var h = $(window).height();
+    $(".info_wrapper").css('height', '0' + "px");
+    $(".information_block").css('opacity', 0).fadeOut(1000, linear);
+    $(".information_block").css('display', none);
+}
+
+/* smooth scroll */
+var scroll = new SmoothScroll('a[data-scroll]', {
+    speed: 2500,
+    easing: 'easeInOutCubic'
+});
