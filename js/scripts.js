4128cfa198d49f562f7afe296c756565a0e64f95
jkenix <80328121+jkenix@users.noreply.github.com>
2022-04-06 23:44:29 +0300

    Добавлено определение query и скрипт фикса меню

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,16 @@
+// Определение Jquery 
+var jquery = require("jquery");
+window.$ = window.jQuery = jquery; // notice the definition of global variables here
+// Функция переключения класса навигации на фиксированный при скролле
+// Импортирование стилей
 import '/styles/wooder_jk_parcel.scss'; // Comment, if don't use parcel
+$(function($) {
+    $(window).scroll(function(){
+        if($(this).scrollTop()>0){
+            $('#navigation').addClass('fixednav');
+        }
+        else if ($(this).scrollTop()<1){
+            $('#navigation').removeClass('fixednav');
+        }
+    });
+  });
