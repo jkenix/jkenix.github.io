16709b130b9224541114c7c4c7ae4884aaff7380
jkenix <80328121+jkenix@users.noreply.github.com>
2022-04-07 23:11:10 +0300

    Добавлен новый скрипт + изм. высоты появления меню

--- js/scripts.js
+++ js/scripts.js
@@ -1,16 +1,18 @@
 // Определение Jquery 
 var jquery = require("jquery");
 window.$ = window.jQuery = jquery; // notice the definition of global variables here
-// Функция переключения класса навигации на фиксированный при скролле
 // Импортирование стилей
 import '/styles/wooder_jk_parcel.scss'; // Comment, if don't use parcel
+// Функция переключения класса навигации на фиксированный при скролле
 $(function($) {
     $(window).scroll(function(){
-        if($(this).scrollTop()>0){
-            $('#navigation').addClass('fixednav');
+        if($(this).scrollTop()>1043){
+            $('#navigation').addClass('__fixednav');
+            $('#sub').addClass('__black');
         }
-        else if ($(this).scrollTop()<1){
-            $('#navigation').removeClass('fixednav');
+        else if ($(this).scrollTop()<1043){
+            $('#navigation').removeClass('__fixednav');
+            $('#sub').removeClass('__black');
         }
     });
-  });
+});
