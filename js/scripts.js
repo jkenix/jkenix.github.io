3ff6c4c93391cfa13ee9d509253dd6e5be8dfe68
jkenix <80328121+jkenix@users.noreply.github.com>
2022-04-24 23:21:27 +0300

    Добавлено объявление bootstrap

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,6 @@
 // Определение Jquery 
 // If you dont' use node.js comment var line
 var jquery = require("jquery"); window.$ = window.jQuery = jquery;  
+var bootstrap = require("bootstrap"); 
 // Импортирование стилей // Import styles
 import '/styles/jk_parcel.scss'; // Comment, if don't use parcel
-// 
-
-$(document).ready(function () {
-    $(window).resize(function () {
-        if($(window).width() <= 449){
-            $('.nav-items').removeClass('nav-items');
-            
-        }
-        else if ($(window).width() >= 450){
-            $('.nav-items').addClass('nav-items');
-        }
-    })
-})
