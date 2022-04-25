e7124efd1e306b903268e58a699a98f887e352aa
jkenix <80328121+jkenix@users.noreply.github.com>
2022-04-25 23:13:32 +0300

    Добавлен скрипт фиксации меню при прокрутке

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,20 @@
-// Определение Jquery 
+// Import styles
+import '/styles/jk_parcel.scss'; // Comment, if don't use parcel
+// Define Jquery 
 // If you dont' use node.js comment var line
 var jquery = require("jquery"); window.$ = window.jQuery = jquery;  
 var bootstrap = require("bootstrap"); 
-// Импортирование стилей // Import styles
-import '/styles/jk_parcel.scss'; // Comment, if don't use parcel
+// Function to switch navigation class to fixed on scroll
+// and change the position of the modal window
+$(function($) {
+    $(window).scroll(function(){
+        if($(this).scrollTop()>400){
+            $('.header').addClass('fixed-top');
+            $('.modal-dialog').addClass('modal-dialog-center');
+        }
+        else if ($(this).scrollTop()<400){
+            $('.header').removeClass('fixed-top');
+            $('.modal-dialog').removeClass('modal-dialog-center');
+        }
+    });
+});
