67df4f5da08a5e460cf41456e1132fe0224320c2
jkenix <80328121+jkenix@users.noreply.github.com>
2022-05-20 23:29:12 +0300

    Неб изм + изменена высота появления скролла

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,7 @@
 // Define Jquery (If you dont' use node.js comment this lines)
 // var jquery = require("jquery"); window.$ = window.jQuery = jquery;  
 // var bootstrap = require("bootstrap"); 
+// var jBox = require("jBox");
 // ==================================================================
 // Jbox
 new jBox('Modal', {
@@ -21,33 +22,34 @@ var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
 })
 // Function to switch navigation class to fixed on scroll
 // and change the position of the modal window 
-$(window).scroll(function(){
-        if($(this).scrollTop()>10){
-            $(function(Showmenu){
-                $('.header').addClass('__scroll');
-                $('.header').addClass('fixed-top');
-                $('.modal-dialog').addClass('modal-dialog-center');
-            });
-        }
-        else if ($(this).scrollTop()<10) {
-            $('.header').removeClass('__scroll');
-            $('.header').removeClass('fixed-top');
-            $('.modal-dialog').removeClass('modal-dialog-center');
-        }
-        // Check page reboot
-        else if (window.innerWidth > 10) {
-            return Showmenu();
-        }
-    });
+// $(window).scroll(function(){
+//         if($(this).scrollTop()>10){
+//             $(function(Showmenu){
+//                 $('.header').addClass('__scroll');
+//                 $('.header').addClass('fixed-top');
+//                 $('.modal-dialog').addClass('modal-dialog-center');
+//             });
+//         }
+//         else if ($(this).scrollTop()<10) {
+//             $('.header').removeClass('__scroll');
+//             $('.header').removeClass('fixed-top');
+//             $('.modal-dialog').removeClass('modal-dialog-center');
+//         }
+//         // Check page reboot
+//         else if (window.innerWidth > 10) {
+//             // return Showmenu();
+//         }
+//     });
 // Function of add button of menu on width < 201px (Work, ONLY if you load page with this width)
 // and form .e-name block interactive
 $(document).ready(function() {
     if (window.innerWidth < 295) {
-            $(".menu").prepend('<a class="page-link __bg-dark-menu"><i class="fa-solid fa-bars"></i> MENU</a>');
-            $(".menu").addClass("menu__active");
+            $(".header-wrapper").prepend('<a class="menu __bg-dark-menu">MENU<i class="fa-solid fa-bars"></i></a>');
+            $(".header-wrapper").addClass("menu__active");
         // Add class, if mouse over menu__active block
         $('.menu__active').mouseover(function() {
-            $('.nav-items a').addClass('page-link __bg-dark-menu');
+            // $('.phone').addClass('page-link __bg-dark-menu'), 
+            // $('.media').addClass('page-link __bg-dark-menu');
         });
     }
     // Function rename default value and add interacive
@@ -59,16 +61,10 @@ $(document).ready(function() {
                 });
         });
 });
-// Function show message, if click on fb logo.
-$(document).ready(function() {
-    $('.fb_logo, .facebook_logo').click(function() { 
-        alert('This is just fb logo from layout. I don"t have fb.');
-    });
-});
 // Function scroll to top
 $(function(){
 	$(window).scroll(function(){
-		if($(window).scrollTop() > 10) {
+		if($(window).scrollTop() > 50) {
 			$('#scroll_top').show();
             $('#scroll_top').css({"display": "flex"},);
 		} else {
