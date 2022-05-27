96b987b720c7dbac4ccc59cd854e7f337737298e
jkenix <80328121+jkenix@users.noreply.github.com>
2022-05-27 23:52:42 +0300

    Изменение отзывчивости скриптов

--- js/scripts.js
+++ js/scripts.js
@@ -23,51 +23,56 @@ var dropdownList = dropdownElementList.map(function (dropdownToggleEl) {
 // Function to switch navigation class to fixed on scroll
 // and change the position of the modal window 
 $(window).scroll(function(){
-        if($(this).scrollTop()>300){
-            $(function(Showmenu){
-                $('.modal-dialog').addClass('modal-dialog-center');
-            });
-        }
-        else if ($(this).scrollTop()<300) {
-            $('.modal-dialog').removeClass('modal-dialog-center');
-        }
-        // Check page reboot
-        else if (window.innerWidth > 300) {
-            return Showmenu();
-        }
-    });
+    if($(this).scrollTop()>300){
+        $(function(Showmenu){
+            $('.modal-dialog').addClass('modal-dialog-center');
+        });
+    }
+    else if ($(this).scrollTop()<300) {
+        $('.modal-dialog').removeClass('modal-dialog-center');
+    }
+    // Check page reboot
+    else if (window.innerWidth > 300) {
+        return Showmenu();
+    }
+});
 // Function of add button of menu on width < 201px (Work, ONLY if you load page with this width)
 // and form .e-name block interactive
 $(document).ready(function() {
     if (window.innerWidth < 295) {
-            $(".menu").prepend('<a class="menu __bg-dark-menu">MENU<i class="fa-solid fa-bars"></i></a>');
-        // Add class, if mouse over menu__active block
+        $(".menu").prepend('<a class="menu __bg-dark-menu">MENU<i class="fa-solid fa-bars"></i></a>');
+    // Add class, if mouse over menu__active block
         $('.menu').mouseover(function() {
             $('.nav-item').addClass('page-link __bg-dark-menu');
         });
     }
     // Function rename default value and add interacive
-        $(function(Ename_active){ 
-            $('.e-name').val('Enter email');
-                // Clear value functions
-                $('.e-name').click(function(){
-                    $(this).val('');
-                });
-        });
+    $(function(Ename_active){ 
+        $('.e-name').val('Enter email');
+            // Clear value functions
+            $('.e-name').click(function(){
+                $(this).val('');
+            });
+    });
 });
 // Function scroll to top
-$(function(){
-	$(window).scroll(function(){
-		if($(window).scrollTop() > 50) {
-			$('#scroll_top').show();
+$(window).scroll(function(){
+	if($(window).scrollTop() > 50) {
+        $(function(Showscroll){
+            $('#scroll_top').show();
             $('#scroll_top').css({"display": "flex"},);
-		} else {
-			$('#scroll_top').hide();
-		}
-	});
-    // If click on h1, page scroll to top
-	$('#scroll_top'), $("h1").click(function(){
-		$('html, body').animate({scrollTop: 0}, 600);
-		return false;
-	});
+        });
+	} 
+    else if ($(this).scrollTop() < 50) {
+		$('#scroll_top').hide();
+	} 
+    // Check page reboot
+    else if (window.innerHeight > 50) {
+        return Showscroll();
+    }
+});
+// If click on h1, page scroll to top
+$('#scroll_top'), $("h1").click(function(){
+	$('html, body').animate({scrollTop: 0}, 600);
+	return false;
 });
