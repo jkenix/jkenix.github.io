c29b6509af731877506b014b9041ae7a88686cf8
jkenix <80328121+jkenix@users.noreply.github.com>
2022-11-23 21:53:25 +0300

    Тестовая сборка версии Toy.stream с хэдером 23/11/2022 21:53

--- src/components/Hamburger.jsx
+++ src/components/Hamburger.jsx
@@ -1,18 +1,5 @@
 import React, { useEffect, useState } from "react";
 
 export default function Hamburger() {
-  const [isActive, setIsActive] = useState(false);
 
-  const handleClick = (event) => {
-    // 👇️ toggle isActive state on click
-    // event.currentTarget.classList.toggle("ham-active");
-    setIsActive((current) => !current);
-  };
-  return (
-    <div
-      className={`hamburger ${isActive ? "ham-active" : ""}`}
-      onClick={handleClick}>
-      <span className="hamburger-item"></span>
-    </div>
-  );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,22 +1,35 @@
 import logo from "../img/Company-logo.svg";
+import { useEffect, useState } from "react";
 import Ccomponent from "./Ccomponent.js";
 import Afcomponent from "./Afcomponent.js";
 import Fcomponent from "./Fcomponent.js";
-import {Link} from "react-router-dom";
+import { Link } from "react-router-dom";
 import Hamburger from "./Hamburger.jsx";
 
 function Header() {
+  const [isActive, setIsActive] = useState(false);
+
+  const handleClick = (event) => {
+    // 👇️ toggle isActive state on click
+    // event.currentTarget.classList.toggle("ham-active");
+    setIsActive((current) => !current);
+  };
   return (
     <>
       <header className="l-header">
-        <Hamburger />
+        <div
+          className={`hamburger ${isActive ? "ham-active" : ""}`}
+          onClick={handleClick}>
+          <span className="hamburger-item"></span>
+        </div>
         <Link className="logo" to="/">
           <img src={logo} alt="Toy.Stream logo" width="135" height="75" />
         </Link>
         {/* <Link className="logo" to="/About">
           sdsdds
         </Link> */}
-        <nav className="header-menu">
+        <nav
+          className={`header-menu ${isActive ? "nav-active" : ""}`} onClick={handleClick}>
           <Link to="/">Главная</Link>
           <Link to="/company">Компания</Link>
           <Link to="/games">Игры</Link>
@@ -25,7 +38,7 @@ function Header() {
           <Link to="/contacts">Контакты</Link>
         </nav>
         <div className="lang-sw">
-        <Link to="/">ENG</Link>
+          <Link to="/">ENG</Link>
         </div>
       </header>
     </>
