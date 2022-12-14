d4c8fcf0289c364db50610b89c55061e29f08371
jkenix <80328121+jkenix@users.noreply.github.com>
2022-12-14 22:34:54 +0300

    Скрипт для доб фона был перемещен в Home.jsx

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,8 +1,6 @@
 // Vanilla scripts js
 // Yellow bg def and switch
 let bodyw = document.querySelector("body");
-bodyw.classList.add("b-ylg-bg");
-
 export default function headerswy() {
   bodyw.classList.toggle("b-ylg-bg");
 }
