016439a698744116ecbb22aa44f110f0b9684902
jkenix <80328121+jkenix@users.noreply.github.com>
2023-01-24 21:56:44 +0300

    Готовы блоки abauth, sponsors + фиксы - Больше модульности коду - Фиксы в скриптах, закрытие по пустоте в меню 24/01/22

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,37 +1,37 @@
-/* Def menu scripts.\*/
-window.onload = function () {
+window.onload = function (RemovePreloader) {
   document.querySelector(".l-preloader").remove("l-preloader");
 };
-// Common show menu script
+/* Def menu scripts.\*/
+// Common show menu variables
 let header = document.querySelector(".l-header");
 let body = document.querySelector("body");
 let ham = document.querySelector(".hamburger");
-
 // Main func active Nav
 ham.onclick = function (NavActive) {
   let headermenu = document.querySelector(".header-menu");
-  let btnhead = document.querySelector(".btn-head");
   ham.classList.toggle("ham-is-active");
   body.classList.toggle("overflow-is-hidden");
   headermenu.classList.toggle("nav-is-active");
-  btnhead.classList.toggle("bh-is-active");
 
   headermenu.addEventListener("click", function () {
     var currentTarget = event.target;
-    // If you want A to be clickable
-    if (currentTarget.tagName === "A") {
+    function CloseMenu() {
       let body = document.querySelector("body");
       body.classList.remove("overflow-is-hidden");
       headermenu.classList.remove("nav-is-active");
       ham.classList.remove("ham-is-active");
-      btnhead.classList.remove("bh-is-active");
+    }
+    CloseMenu(); // Close menu, if click on empty space hmenu
+    // Close menu, if click on a elements of hmenu
+    if (currentTarget.tagName === "A") {
+      CloseMenu();
     }
   });
 };
-
+// Show/Remove scroll
 window.addEventListener(
   "scroll",
-  function () {
+  function (Scroll) {
     if (window.scrollY > 100) {
       header.classList.add("header-scroll");
     } else if (window.scrollY < 100) {
