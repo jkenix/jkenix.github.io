50bc2025c37b9277ccf8d1c3d05a28d6447f1ed7
jkenix <80328121+jkenix@users.noreply.github.com>
2023-03-05 11:14:05 +0300

    Убрано разделение бандлов

--- prod.config.js
+++ prod.config.js
@@ -38,21 +38,21 @@ module.exports = {
       chunks: "all",
       maxInitialRequests: Infinity,
       minSize: 0,
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name(module) {
-            // получает имя, то есть node_modules/packageName/not/this/part.js
-            // или node_modules/packageName
-            const packageName = module.context.match(
-              /[\\/]node_modules[\\/](.*?)([\\/]|$)/
-            )[1];
-            // имена npm-пакетов можно, не опасаясь проблем, использовать
-            // в URL, но некоторые серверы не любят символы наподобие @
-            return `npm.${packageName.replace("@", "")}`;
-          },
-        },
-      },
+      // cacheGroups: {
+      //   vendor: {
+      //     test: /[\\/]node_modules[\\/]/,
+      //     name(module) {
+      //       // получает имя, то есть node_modules/packageName/not/this/part.js
+      //       // или node_modules/packageName
+      //       const packageName = module.context.match(
+      //         /[\\/]node_modules[\\/](.*?)([\\/]|$)/
+      //       )[1];
+      //       // имена npm-пакетов можно, не опасаясь проблем, использовать
+      //       // в URL, но некоторые серверы не любят символы наподобие @
+      //       return `npm.${packageName.replace("@", "")}`;
+      //     },
+      //   },
+      // },
     },
     // splitChunks: {
     //   chunks: "async",
