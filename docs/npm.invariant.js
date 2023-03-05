de994450b754a25861338e830fc087fe30d55e38
jkenix <80328121+jkenix@users.noreply.github.com>
2023-03-05 11:07:35 +0300

    Тест сборка с разделением бандлов 05/03/23

---
+++ docs/npm.invariant.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[381],{1143:e=>{e.exports=function(e,n,r,i,o,a,t,s){if(!e){var f;if(void 0===n)f=new Error("Minified exception occurred; use the non-minified dev environment for the full error message and additional helpful warnings.");else{var u=[r,i,o,a,t,s],l=0;(f=new Error(n.replace(/%s/g,(function(){return u[l++]})))).name="Invariant Violation"}throw f.framesToPop=1,f}}}}]);
+//# sourceMappingURL=npm.invariant.js.map
